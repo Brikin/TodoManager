887a86c0a21af5c05769132be5697a8d3fe82b1b
Ruslan Brikin <ruslan.brikin@icloud.com>
2017-11-28 10:10:01 +0100

    Implement TaskList TaskDetails

---
+++ ToDoList/TasksDetailsController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsDetailsController.swift
+//  ToDoList
+//
+//  Created by Ruslan on 28/11/2017.
+//  Copyright © 2017 Ruslan. All rights reserved.
+//
+
+import UIKit
+
+class TasksDetailsController: UIViewController {
+    
+}
---
+++ ToDoList/TasksListTableControllert.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsList.swift
+//  ToDoList
+//
+//  Created by Ruslan on 28/11/2017.
+//  Copyright © 2017 Ruslan. All rights reserved.
+//
+
+import UIKit
+
+class TasksListTableController: UIViewController {
+    
+}
